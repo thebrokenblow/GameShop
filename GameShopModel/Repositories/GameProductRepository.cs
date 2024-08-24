36220d39f2bf5c91ec1eb46d6008526dfdbad72f
Artem <artkrasov@gmail.com>
2024-08-24 23:46:32 +0300

    Немного изменил логику обновления и удаления

--- GameShopModel/Repositories/GameProductRepository.cs
+++ GameShopModel/Repositories/GameProductRepository.cs
@@ -19,28 +19,27 @@ public class GameProductRepository(GameShopContext gameShopContext) : IGameProdu
         await gameShopContext.SaveChangesAsync();
     }
 
-    public async Task RemoveGameProductAsync(int id)
-    {
-        var deletingGameProduct = await gameShopContext.GameProducts
-            .FirstAsync(gameProduct => gameProduct.Id == id);
+    public async Task RemoveGameProductAsync(int id) =>
+        await gameShopContext.GameProducts
+        .Where(gameProduct => gameProduct.Id == id)
+        .ExecuteDeleteAsync();
 
-        gameShopContext.GameProducts.Remove(deletingGameProduct);
-        await gameShopContext.SaveChangesAsync();
-    }
-
-    public async Task EditGameProductAsync(int id, GameProduct gameProduct)
-    {
-        var editingGameProduct = await gameShopContext.GameProducts
-            .FirstAsync(gameProduct => gameProduct.Id == id);
-
-        editingGameProduct.PresentationImageUrl = gameProduct.PresentationImageUrl;
-        editingGameProduct.Title = gameProduct.Title;
-        editingGameProduct.Description = gameProduct.Description;
-        editingGameProduct.Price = gameProduct.Price;
-        editingGameProduct.ReleaseDate = gameProduct.ReleaseDate;
-        editingGameProduct.Genre = gameProduct.Genre;
-        editingGameProduct.ImagesUrl = gameProduct.ImagesUrl;
-
-        await gameShopContext.SaveChangesAsync();
-    }
+    public async Task EditGameProductAsync(int id, GameProduct gameProduct) =>
+        await gameShopContext.GameProducts
+            .Where(editingGameProduct => editingGameProduct.Id == id)
+            .ExecuteUpdateAsync(s => s
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.PresentationImageUrl, gameProduct => gameProduct.PresentationImageUrl)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.Title, gameProduct => gameProduct.Title)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.Description, gameProduct => gameProduct.Description)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.Price, gameProduct => gameProduct.Price)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.ReleaseDate, gameProduct => gameProduct.ReleaseDate)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.Genre, gameProduct => gameProduct.Genre)
+                .SetProperty(
+                    editingGameProduct => editingGameProduct.ImagesUrl, gameProduct => gameProduct.ImagesUrl));
 }
